c3339b27cb53a05f3c466ddef93e81fa86762c92
Bointon <bointon@gmail.com>
2021-01-02 13:27:55 +0000

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,93 @@
+"""
+This will show some Linux Statistics on the attached display. Be sure to adjust
+to the display you have connected. Be sure to check the learn guides for more
+usage information.
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+"""
+
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.ili9341 as ili9341
+
+#Initialise the LCD screen
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+BAUDRATE = 24000000
+
+spi = board.SPI()
+
+disp = ili9341.ILI9341(
+    spi,
+    rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+
+image = Image.new("RGB", (width, height))
+
+#Standard runtime initialisation
+splashFlag = true
+splashTimer = 500
+splashTimerCnt = 0
+menuFlag = false
+
+#Standard runtime
+
+while True:
+    
+    if splashFlag == true:
+        #The splash screen 
+        splashScreen()
+        if splashTimer < splashTimerCnt:
+            splashFlag = false
+        splashTimerCnt += 1
+    if menuFlag == true:
+        #The menu screen
+
+    #cycle timer
+    time.sleep(0.1)
+
+
+#Splash screen logo
+def splashScreen():
+    
+    image = Image.open("logo.png")
+
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+
+
+    time.sleep(0.1)
