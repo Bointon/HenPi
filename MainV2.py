32945cf81ae0e866f84edac5063b2a9a9b5e226c
Bointon <bointon@gmail.com>
2021-01-10 16:55:42 +0000

    Update MainV2.py

--- MainV2.py
+++ MainV2.py
@@ -319,12 +319,12 @@ def settingsScreen(menuSelect):
     print(settings[2])
     print(settings[3])
 
-    for i in range(0,5):
+    for i in range(0,4):
         draw.text((2*padding, 4+(i+1)*36), textArray[i] , font=font, fill=0)
-        #if i < 5:
-        #   print(settings[i])
-        #    print(font.getsize(textOptions[i][settings[i]])[0])
-        #    print(textOptions[i][settings[i]])
+        if i < 4:
+
+        print(font.getsize(textOptions[i][settings[i]])[0])
+        print(textOptions[i][settings[i]])
         #    draw.text(((font.getsize(textOptions[i][settings[i]])[0]), 4+(i+1)*36), textOptions[i][settings[i]] , font=font, fill=0)
 
     disp.image(image)
