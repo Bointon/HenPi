6148056ed8472287be549d43d92dd13ecbb43907
Bointon <bointon@gmail.com>
2021-01-10 15:07:05 +0000

    Update MainV2.py

--- MainV2.py
+++ MainV2.py
@@ -227,7 +227,6 @@ def menuScreen(menuSelect):
     for i in range(0,5):
         draw.text((2*padding, 4+(i+1)*36), textArray[i] , font=font, fill=0)
 
-   
     disp.image(image)
 
 #About screen
@@ -244,7 +243,7 @@ def aboutScreen():
     draw.text(((disp.height-font.getsize(titleText)[0])/2, 4), titleText , font=font, fill="#FFFFFF")
 
     textArray = ["Henniker Scientific","HW Ver","SW Ver","Support"]
-    for i in range(0,5):
+    for i in range(0,4):
         draw.text((2*padding, 4+(i+1)*36), textArray[i] , font=font, fill=0)
 
     disp.image(image)
